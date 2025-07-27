6cef62621c1e72ebd2f3ed0b8f8c16d6b853f4d0
Maurilio Souza Menezes <mauriliosm25@gmail.com>
2025-07-26 22:57:13 -0300

    feat: initial config

---
+++ afps/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+
+    return (
+        <div className="flex flex-col items-center justify-conter">
+            <h1 className="text-center font-bold mt-9 text-6xl "> Página 404 não encontrada!</h1>
+        </div>
+    )
+}
